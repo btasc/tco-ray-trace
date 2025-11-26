95af42d2df7e79bd42c6bf453d9baac82bd0e4a4
btasc <brianjtascone@gmail.com>
2025-11-26 12:38:49 +0000

    Made basic struct

--- src/engine/model.rs
+++ src/engine/model.rs
@@ -1,4 +1,19 @@
 pub struct Model {
     name: Option<String>,
-    path: std::path::PathBuf,
+    verticies: Vec<f32>,
+    triangles: Vec<[u32, 3]>,
+}
+
+impl Model {
+    pub fn new() -> Self {
+        Self {
+            name: None,
+            verticies: Vec::new(),
+            triangles: Vec::new(),
+        }
+    }
+
+    pub fn from_obj(obj_file: String) -> Self {
+        todo!()
+    }
 }
